9d6238b5958c201d42d9785d9407bb7ab26d4e70
Hoang Duc <huyduk19@gmail.com>
2020-09-26 11:29:34 +0700

    day code sua hovẻ lan 2

--- assets/effect.js
+++ assets/effect.js
@@ -4,7 +4,7 @@ $(window).load(function() {
 });
 $('document').ready(function() {
 
-    $(body).hover(function() {
+    $("body").hover(function() {
         var audio = $('.song')[0];
         audio.play();
     })
