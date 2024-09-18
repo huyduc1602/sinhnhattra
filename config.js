bff7e3f91843a5870febd41aab166f141185df15
Duc_dev <nesv036@gmail.com>
2024-09-18 11:39:00 +0700

    first commit

--- config.js
+++ config.js
@@ -3,16 +3,14 @@ var config = {
     //Độ dài của câu có thể là bất kỳ, bạn có thể viết mười câu hoặc hai mươi câu.
     // Cố gắng không vượt quá 15 từ trong mỗi câu, nếu không hiệu quả hiển thị có thể không tốt lắm
     texts: [
-        "Gửi cho chị", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "Nguyễn Thị Ngọc Thủy", // 同上...
-        "Người mà em yêu quý",
-        "Sinh nhật tròn 26 tuổi của chị",
-        "Mặc dù e không thể ở cạnh chị",
-        "Nhưng e vẫn luôn mong mọi thứ tốt đẹp nhất với chị",
-        "Cảm ơn chị thời gian qua",
-        "Một người chị tuyệt vời",
-        "Cảm ơn đã có chị xuất hiện",
-        "Chị yêu quý",
+        "Gửi cho cậu", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Phạm Thanh Trà", // 同上...
+        "Người mà tớ yêu quý",
+        "Sinh nhật tròn 25 tuổi của cậu",
+        "Mặc dù tớ không ở gần đó",
+        "Nhưng e vẫn luôn mong mọi thứ tốt đẹp nhất với cậu",
+        "Chúc cậu sinh nhật vui vẻ và luôn hạnh phúc, đạt được nhiều thành công trong cuộc sống nha",
+        "Trà yêu quý",
         "Gửi từ Hoàng Huy Đức",
     ],
     /**
@@ -25,7 +23,7 @@ var config = {
      * Mẹo: Hình tốt nhất nên dùng hình vuông hoặc gần vuông thì trông đẹp hơn
      */
     imgs: {
-        "Chị yêu quý": "chi_thuy.jpg",
+        "Trà yêu quý": "tra.jpg",
         // "Chị yêu quý": "./imgs/birthday.jpg",
     },
     // Mô tả văn bản nút, sau đây là văn bản nút mặc định, bằng tiếng Anh, bạn có thể thay đổi nó thành văn bản yêu thích của mình
@@ -37,6 +35,6 @@ var config = {
         cake_fadein: "Có thiếu gì không nhỉ?",
         light_candle: "Thổi nến nhé",
         wish_message: "Chúc mừng sinh nhật!",
-        story: "Một tin nhắn cho chị",
+        story: "Một tin nhắn cho cậu",
     }
 };
